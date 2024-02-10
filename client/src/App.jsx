5d9704013d22d8e74d3c6847591db67fa71911e2
Debayan <debayanghosh408@gmail.com>
2024-02-10 23:13:34 +0530

    completed the header section of the project and installed the flowbite and some project based components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <div className='text-red-300'>
-      This is my app
-    </div>
+    <Header/>
   )
 }
