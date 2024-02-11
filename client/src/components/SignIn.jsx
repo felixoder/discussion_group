a98d8f2f075789be32a7754cdae339557e9c76c1
Debayan <debayanghosh408@gmail.com>
2024-02-11 23:22:36 +0530

    the sign-in functionality is set up and the react redux and redux toolkit is properly set up

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,9 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import {useState} from "react";
+import { Link , useNavigate } from "react-router-dom";
 import { Label, TextInput , Button } from "flowbite-react";
+import {useDispatch , useSelector} from 'react-redux'
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice";
 
 
 export default function SignIn() {
+  const [formData , setFormData] = useState({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const handleChange =(e)=>{
+    setFormData({...formData , [e.target.id]: e.target.value.trim()})
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    if(!formData.email || !formData.password){
+      return dispatch(signInFailure('Please fill the all fields'))
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(signInFailure(data.message))
+      }
+      if(res.ok){
+        dispatch(signInSuccess(data));
+        navigate('/')
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message))
+      
+    }
+  }
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 '>
@@ -23,13 +57,14 @@ export default function SignIn() {
         {/* right */}
 
         <div className='flex-1'>
-          <form className='flex flex-col gap-4' >
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
             <div>
               <Label value='Your email' />
               <TextInput
                 type='email'
                 placeholder='name@company.com'
                 id='email'
+                onChange={handleChange}
                 
               />
             </div>
@@ -39,12 +74,14 @@ export default function SignIn() {
                 type='password'
                 placeholder='**********'
                 id='password'
+                onChange={handleChange}
                 
               />
             </div>
             <Button
               gradientDuoTone='purpleToPink'
               outline
+              type="submit"
             
             >
               Sign-In
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {store , persistor} from './redux/store.js'
+import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from './components/ThemeProvider.jsx'
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <ThemeProvider>
+        <BrowserRouter>
+
+        <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  </PersistGate>
+);
