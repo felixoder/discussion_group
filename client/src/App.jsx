fb792b2eeb75de46e960c51ca4a9659edaeddf4f
Debayan <debayanghosh408@gmail.com>
2024-02-13 03:23:51 +0530

    delete user UI is set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
       <Header />
       <Routes>
         
-        <Route path="/" element={<Home/>} />
+        <Route  path="/" element={<Home/>} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,14 @@
 import { useSelector , useDispatch } from "react-redux";
 import { SlCalender } from "react-icons/sl";
-import { Button } from "flowbite-react";
+import { Button, Modal } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import {useState} from 'react'
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const dispatch =  useDispatch()
+  const [showModel , setShowModel] = useState(false)
   const handleSignOut = async ()=>{
     try {
       const res = await fetch('/api/user/signout',{
@@ -25,6 +28,28 @@ export default function DashProfile() {
       console.log(error.message);
       
     }
+  }
+  const handleDeleteUser=async ()=>{
+    setShowModel(false);
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+  
+      })
+      const data = await res.json();
+      if(!res.ok){
+        dispatch(deleteUserFailure(data.message));
+      }
+      else{
+        dispatch(deleteUserSuccess(data))
+      }
+      
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+      
+    }
+  
   }
   return (
     <div className="">
@@ -58,11 +83,26 @@ export default function DashProfile() {
       </div>
       <div className="">
       <div className="text-red-700 flex justify-between mt-10 ml-5 mr-5 font-semibold">
-        <span className="cursor-pointer ">Delete Account</span>
+        <Button gradientDuoTone='purpleToBlue' outline   onClick={()=>setShowModel(true)} className="cursor-pointer">Delete Account</Button>
         <Button type="submit" className="cursor-pointer" onClick={handleSignOut} gradientDuoTone='purpleToPink' outline >Sign-Out</Button>
       </div>
       </div>
+      <Modal show={showModel} onClose={()=> setShowModel(false)} popup size='md'>
+        <Modal.Header/>
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto"/>
+            <h3 className="mb-5 text-lg text-grey-500 dark:text-grey-400">Are You sure you want to delete your account</h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handleDeleteUser}>Yes I am sure</Button>
+              <Button color="grey" onClick={()=> setShowModel(false)}>No, Cancel </Button>
+            </div>
+          </div>
+        </Modal.Body>
+
+      </Modal>
       
     </div>
+    
   );
 }
