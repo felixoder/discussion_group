709b9f7e129d20ae11dc138448ddeef1840604f8
Debayan <debayanghosh408@gmail.com>
2024-02-14 20:13:48 +0530

    the answer model schema is set up successfully

---
+++ api/models/answer.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const answerSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    numberOfLikes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Answer = mongoose.model('Answer', answerSchema);
+
+export default Answer;
