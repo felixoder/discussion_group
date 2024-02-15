e78ddb57ef3aa3b73dfbdcf1a17b70296c412178
Debayan <debayanghosh408@gmail.com>
2024-02-15 15:04:16 +0530

    your posts is fixed

--- client/src/components/YourPost.jsx
+++ client/src/components/YourPost.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import { Modal, Table, Button } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export default function YourPost() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchUserPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+          
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    if (currentUser) {
+      fetchUserPosts();
+    }
+  }, [currentUser]);
+
+  
+
   return (
-    <div>
-      YourPost
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 '>
+      <h1 className='text-center font-semibold text-3xl mb-5'>Your Questions</h1>
+      {currentUser && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+         
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Tag</Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body key={post._id} className='divide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+               
+                  <Table.Cell>
+                    <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.tag}</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+          
+        </>
+      ) : (
+        <p>You have no Questions yet!</p>
+      )}
     </div>
-  )
+  );
 }
